00d0227987b2dd889c05da36b051aad7d6faa7bd
Richard Simms <rgsimms87@gmail.com>
2014-07-31 15:12:04 +1000

    Updated stuff
    updated my css and js

--- RallyGoodReport.js
+++ RallyGoodReport.js
@@ -1,5 +1,25 @@
+function displayCategories() {
+            
+    var config = { 
+            title: "Hello World",
+			width: 300, id:"helloWorld",
+			content: "<div>Hello World!</div>"
+			}
+
+	var dialog = new rally.sdk.ui.basic.Dialog({ 
+            title: "Hello World",
+			width: 300, id:"helloWorld",
+			content: "<div>Hello World!</div>"
+			})
+
+	}
+
+	dialog.display;
+
 function onLoad() {
-    //Add app code here
+
+
 }
+
 
 rally.addOnLoad(onLoad);
