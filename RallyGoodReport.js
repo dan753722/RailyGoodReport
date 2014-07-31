ccd9e89a90a0c352d23772a70fd2aa7396a4e9e2
Daniel Sun <H.Sun@qsrinternational.com>
2014-07-31 15:08:15 +1000

    Commit Rally Good Report code framework

--- RallyGoodReport.js
+++ RallyGoodReport.js
@@ -1,5 +1,67 @@
 function onLoad() {
     //Add app code here
+    var self = this,
+    	GoodRallyReportProperties = {
+    		Consts : {
+    			PIE_CHART : "pieChartDiv",
+    			ChartOptions : {
+	    			PIE  	  : "pie",
+    				BAR 	  : "bar",
+    				LINE 	  : "line",
+    				BURN_DOWN : "burndown"
+    			}
+    		}
+    	},
+    	ShowPie = function() {
+    		var rallyDataSource = new rally.sdk.data.RallyDataSource('__WORKSPACE_OID__',
+                               '__PROJECT_OID__',
+                               '__PROJECT_SCOPING_UP__',
+                               '__PROJECT_SCOPING_DOWN__');
+       		var pieConfig = {
+            	type : "Defect",
+           		attribute: "Priority",
+           		title : "Defects by Priority",
+           		height : 200,
+           		width : 200
+         	};
+       		var pieChart = new rally.sdk.ui.PieChart(pieConfig, rallyDataSource);
+	        pieChart.display(GoodRallyReportProperties.Consts.PIE_CHART);
+    	},
+	 	GoodRallyReportHelpers = {
+    		Helpers : {
+				GenerateNavPie: function() {
+    					$(".charts div").addClass(GoodRallyReportProperties.Consts.PIE_CHART)
+    									.attr("id", GoodRallyReportProperties.Consts.PIE_CHART);
+
+    					ShowPie();
+    			}
+    		},
+    		Events : {
+    			ButtonEvent : {
+    				GenerateNavChartClicked : function() {
+    					var chartType = $(".chart-option").val();
+    					if (chartType === GoodRallyReportProperties.Consts.ChartOptions.PIE) {
+    						GoodRallyReportHelpers.Helpers.GenerateNavPie();
+    					}
+
+    					if (chartType === GoodRallyReportProperties.Consts.ChartOptions.BAR) {
+
+    					}
+
+    					if (chartType === GoodRallyReportProperties.Consts.ChartOptions.LINE) {
+
+    					}
+
+    					if (chartType === GoodRallyReportProperties.Consts.ChartOptions.BURN_DOWN) {
+
+    					}
+    				}
+    			}
+    		},
+
+    	};
+
+    $(".btn-Generate-nav-chart").click(GoodRallyReportHelpers.Events.ButtonEvent.GenerateNavChartClicked);
 }
 
 rally.addOnLoad(onLoad);
