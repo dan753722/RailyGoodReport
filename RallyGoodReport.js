42a755beda95fac4024af2506e97637f5a89ed22
Lachlan Brooks <l.brooks@qsrinternational.com>
2014-08-01 10:57:45 +1000

    fixed showPieChartWIthFiltering
    Conflicts:

--- RallyGoodReport.js
+++ RallyGoodReport.js
@@ -1,35 +1,40 @@
 function onLoad() {
     //Add app code here
     var self = this,
-    	GoodRallyReportProperties = {
-    		Consts : {
-    			PIE_CHART : "pieChartDiv",
-    			ChartOptions : {
-	    			PIE  	  : "pie",
-    				BAR 	  : "bar",
-    				LINE 	  : "line",
-    				BURN_DOWN : "burndown"
-    			},
+        GoodRallyReportProperties = {
+            Consts : {
+                PIE_CHART : "pieChartDiv",
+                ChartOptions : {
+                    PIE       : "pie",
+                    BAR       : "bar",
+                    LINE      : "line",
+                    BURN_DOWN : "burndown"
+                },
                 Attributes : {
                     STATE : "State"
+                },
+                Buttons : {
+                    GO_BTN : "Go!"
                 }
-    		}
-    	},
-
-    	ShowPie = function(objectType, selectedAttribute) {
-    		var rallyDataSource = new rally.sdk.data.RallyDataSource('__WORKSPACE_OID__',
+            }
+        },
+        ShowPie = function(objectType, selectedAttribute) {
+            var rallyDataSource = new rally.sdk.data.RallyDataSource('__WORKSPACE_OID__',
                                '__PROJECT_OID__',
                                '__PROJECT_SCOPING_UP__',
                                '__PROJECT_SCOPING_DOWN__'),
-       		    pieConfig = {
+                pieConfig = {
                     type : objectType,
                     attribute: selectedAttribute,
                     title : objectType + " by " + selectedAttribute,
                     height : 200,
                     width : 200
-                };
-    	},
+                },
+                pieChart = new rally.sdk.ui.PieChart(pieConfig, rallyDataSource);
 
+            pieChart.addEventListener("onSliceClick", GoodRallyReportHelpers.Events.ClickEvent.OnPieSliceClicked);
+            pieChart.display(GoodRallyReportProperties.Consts.PIE_CHART);
+        },
 
         ShowPieWithFiltering = function(objectType, selectedAttribute, selectedAttributeValue, categorization)
         {
@@ -87,51 +92,71 @@ function onLoad() {
                         highChartInputDataIndex++;
                     }
 
-
-
-                    categorizedResults = categorizedResults(results, categorizedby);
-                    //var pie = initiateHighChart(categorizedResults;
-                    //pie.display();
+                    // ADD HIGHCHART CALL HERE
                 };
                 rallyDataSource.findAll(queryConfig, queryCallBack);
         },
 
+        displayCategories = function() {
+            var dialog = new rally.sdk.ui.basic.Dialog({ 
+                title: "Select a category",
+                width: 300, 
+                content: "  <div id='categories'><h1 class='rally-good-title'>Categorise by:</h1><select class='categories-dropdown'><option value='severity'>Severity</option><option value='priority'>Priority *COMING SOON*</option><option value='owner'>Owner *COMING SOON*</option></select></div>",
+                closable: true,
+                buttons: [GoodRallyReportProperties.Consts.Buttons.GO_BTN]
+            });
 
-	 	GoodRallyReportHelpers = {
-    		Helpers : {
-				GenerateNavPie: function() {
-    					$(".charts div").addClass(GoodRallyReportProperties.Consts.PIE_CHART)
-    									.attr("id", GoodRallyReportProperties.Consts.PIE_CHART);
+            dialog.addEventListener("onButtonClick", GoodRallyReportHelpers.Events.ClickEvent.OnDialogButtonClicked);
+            dialog.display();
+        },
+        GoodRallyReportHelpers = {
+            Helpers : {
+                GenerateNavPie: function() {
+                        $(".charts div").addClass(GoodRallyReportProperties.Consts.PIE_CHART)
+                                        .attr("id", GoodRallyReportProperties.Consts.PIE_CHART);
 
-    					//ShowPie($(".object-type-option").val(), GoodRallyReportProperties.Consts.Attributes.STATE);
-                        ShowPieWithFiltering($(".object-type-option").val(), GoodRallyReportProperties.Consts.Attributes.STATE, "Open", "Priority");
-    			}
-    		},
-    		Events : {
-    			ButtonEvent : {
-    				GenerateNavChartClicked : function() {
-    					var chartType = $(".chart-option").val();
-    					if (chartType === GoodRallyReportProperties.Consts.ChartOptions.PIE) {
-    						GoodRallyReportHelpers.Helpers.GenerateNavPie();
-    					}
+                        ShowPie($(".object-type-option").val(), GoodRallyReportProperties.Consts.Attributes.STATE);
+                }
+            },
+            Events : {
+                ButtonEvent : {
+                    GenerateNavChartClicked : function() {
+                        var chartType = $(".chart-option").val();
+                        if (chartType === GoodRallyReportProperties.Consts.ChartOptions.PIE) {
+                            GoodRallyReportHelpers.Helpers.GenerateNavPie();
+                        }
 
-    					if (chartType === GoodRallyReportProperties.Consts.ChartOptions.BAR) {
+                        if (chartType === GoodRallyReportProperties.Consts.ChartOptions.BAR) {
 
-    					}
+                        }
 
-    					if (chartType === GoodRallyReportProperties.Consts.ChartOptions.LINE) {
+                        if (chartType === GoodRallyReportProperties.Consts.ChartOptions.LINE) {
 
-    					}
+                        }
 
-    					if (chartType === GoodRallyReportProperties.Consts.ChartOptions.BURN_DOWN) {
+                        if (chartType === GoodRallyReportProperties.Consts.ChartOptions.BURN_DOWN) {
 
-    					}
-    				}
-    			}
-    		}
-    	};
+                        }
+                    }
+                },
+                
+                ClickEvent : {
+                    OnPieSliceClicked : function(p, eventArgs) {
+                        displayCategories();
+                    },
+                    OnDialogButtonClicked : function(dialog, eventArgs) {
+                        if (eventArgs.button === GoodRallyReportProperties.Consts.Buttons.GO_BTN) {
+                            // TODO:
+                        }
+                        
+                        dialog.destroy();
+                    }
+                }
+            },
+
+        };
 
     $(".btn-Generate-nav-chart").click(GoodRallyReportHelpers.Events.ButtonEvent.GenerateNavChartClicked);
-}
 
+}
 rally.addOnLoad(onLoad);
